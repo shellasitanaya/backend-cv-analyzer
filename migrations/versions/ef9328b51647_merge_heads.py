052bb2a43ba9a66f1463b8735888519f6dbb9b35
shellyOei <oeishelly15@gmail.com>
2025-10-21 10:46:24 +0700

    fix: delete unnecessary services

---
+++ migrations/versions/ef9328b51647_merge_heads.py
@@ -0,0 +1,24 @@
+"""Merge heads
+
+Revision ID: ef9328b51647
+Revises: 8bd14f33f89b, c7be222a37c7
+Create Date: 2025-10-21 10:32:38.035069
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ef9328b51647'
+down_revision = ('8bd14f33f89b', 'c7be222a37c7')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
